e6f1422a4ea7f031bc5d2cd9f32c3d01c576c7b9
jason <2B3>
2021-09-05 00:32:05 -0400

    commit 236 : dispatch actions with payloads

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,24 @@
 import classes from "./Counter.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
   //  grab the part of the state object you need
-  let counterVal = useSelector((state) => state.counter); 
+  let counterVal = useSelector((state) => state.counter);
+  // Give yourself the ability to use the reducer's dispatch function
+  const dispatch = useDispatch();
+  const up = () => dispatch({ type: "increment" });
+  const down = () => dispatch({ type: "decrement" });
   const toggleCounterHandler = () => {};
-
+  const upMore = () => dispatch({ type: "increment more", amount: 5 });
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counterVal}</div>
+      <div>
+        <button onClick={up}>Increase by 1</button>
+        <button onClick={upMore}>Increase by 5</button>
+        <button onClick={down}>Decrease by 1</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/store-center.js
+++ src/store/store-center.js
@@ -1,13 +1,15 @@
 import { createStore } from "redux";
 // Create a store, then define your reducer function:
-const counterReducer= function(state = { counter: 0 }, action) {
+const counterReducer = function (state = { counter: 0 }, action) {
   switch (action.type) {
     case "increment":
       return { counter: state.counter + 1 };
     case "decrement":
       return { counter: state.counter - 1 };
+    case "increment more":
+      return { counter: state.counter + action.amount };
   }
   return state;
-}
+};
 const store = createStore(counterReducer);
 export default store;
